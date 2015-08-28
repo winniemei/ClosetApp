b73424074c38008f7e392eadbe7b76e9326365c2
Winnie Mei <wmei91@gmail.com>
2015-08-27 22:34:06 -0400

    basic functionality

---
+++ closet/app/assets/javascripts/backbone/views/outfitPreview.js
@@ -0,0 +1,22 @@
+App.Views.OutfitPreview = Backbone.View.extend({
+	initialize: function(){
+		console.log("Outfit preview created");
+		this.template = HandlebarsTemplates['outfitlist'];
+		this.render();
+		this.listenTo(this.model, 'change', this.render);
+		this.listenTo(this.model, 'remove', this.remove)
+	},
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+	},
+	showOutfitView: function(){
+		App.outfitView.setOutfit(this.model);
+	},
+	destroyOutfit: function(){
+		this.model.destroy();
+	},
+	events: {
+		'click p': 'showOutfitView',
+		'click span.delete': 'destroyOutfit'
+	}
+})
